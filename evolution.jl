bc0e770ac83b56f81fb4387d5923630bc71e1508
jankiel <michal7410@gmail.com>
2015-08-14 20:01:13 +0200

    type Tounament, type Roulette (stub only, commented out)

--- evolution.jl
+++ evolution.jl
@@ -2,14 +2,21 @@ type Individual
     valid_genome::Array{Float64, 1}  # genome used for validation
     chromosome::Array{Float64, 1}
     mutation_chance::Float64
-    function Individual(valid_genome, chromosome::Array{Float64, 1}, mutation_chance=0.1)
+    rating::Float64
+
+    function Individual(valid_genome, chromosome::Array{Float64, 1}, mutation_chance=0.1, rating=0.0)
         this = new(valid_genome, chromosome, mutation_chance)
         repair!(this)
         return this
     end
-    
-
 end
+
+
+<(self::Individual, other::Individual) = self.rating < other.rating
+
+
+==(self::Individual, other::Individual) = self.rating == other.rating
+
 
 function mutate!(self::Individual)
     if rand() >= self.m_chance
@@ -22,6 +29,7 @@ function mutate!(self::Individual)
     self.chromosome = vcat(self.chromosome[1:i], center, self.chromosome[j:end])
 end
 
+
 function mutate(self::Individual)
     chromosome_copy = self.chromosome
     mutate!(self)
@@ -30,12 +38,14 @@ function mutate(self::Individual)
     return another
 end
 
+
 function crossover(self::Individual, other::Individual)
     chromosome1, chromosome2 = _cross_over_chromosomes(self, other)
     i1 = Individual(self.valid_genome, chromosome1, self.m_chance)
     i2 = Individual(self.valid_genome, chromosome2, self.m_chance)
     return i1, i2
 end
+
 
 function _cross_over_chromosomes(ch1::Array{Float64}, ch2::Array{Float64}) #krzyżowanie chromosomów 29216.4532612964 27748.70957813485
     i = rand(1:length(ch1) - 3)
@@ -53,6 +63,7 @@ function _cross_over_chromosomes(ch1::Array{Float64}, ch2::Array{Float64}) #krzy
     child2 = vcat(start, middle, ending)
     return child1, child2
 end
+
 
 function repair!(self::Individual)  # gets rid of duplicates
     doubled = Dict(Float64, Int)
@@ -80,4 +91,43 @@ function repair!(self::Individual)  # gets rid of duplicates
         end
     end
 end
-print("wat")
+
+
+type Tournament
+    individuals::Array{Individual, 1}
+    delete::Bool
+    #list = uporządkowana tablica osobników
+    function Tournament(list::Array{Individual, 1}, delete=false) = new([i for i in list], delete)
+end
+
+
+function getbest(self::Tournament, groupsize=4)
+    group = [self.indv[rand(1:length(self.individuals)] for ind in rand(1:groupsize)]
+    best = indmin(group, key=key_cfn)
+    if self.delete:
+        del self.indv[self.indv.index(best)]
+    return best
+end
+
+
+# type Roulette
+#     probs::Array{Float64, 1}
+#     individuals::Array{Individual, 1}
+
+#     function Roulette(self, individuals):#list = uporządkowana tablica osobników
+#         max_rating = individuals[end].rating
+#         sum_rating = sum([max_rating - i.rating for i in individuals])
+#         #[sum([(max - j + 1)/(wat + 1) for j in a[:i+1]]) for i in range(len(a))]
+#         probs = [sum([(max_rating - j.rating + 1)/(sum_rating + 1) for j in individuals[:i+1]])
+#                       for i in range(len(individuals))] # suma prawdopodob.
+#         individuals = [self.probs[i]: individuals[i] for i in range(len(individuals))]
+
+
+#     def next(self):
+#         pick = random.uniform(0, self.probs[-1])#losuj liczbę z zakresu (0, suma z długości tras)
+#         from bisect import bisect
+#         index = bisect(self.probs, pick)
+#         if index < 0 or index >= len(self.probs):
+#             raise Exception('Roulette.next bisect: out of range index=%d'%index)
+#         return self.individuals[self.probs[index]]
+# end
